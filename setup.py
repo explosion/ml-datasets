0768a3445990dfbc3dc1658873928403e8382c23
Ines Montani <ines@ines.io>
2019-12-28 22:21:53 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@
 # coding: utf-8
 
 if __name__ == "__main__":
-    from setuptools import setup
+    from setuptools import setup, find_packages
 
-    setup(name="ml_datasets")
+    setup(name="ml_datasets", packages=find_packages())
