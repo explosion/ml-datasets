53d103cdd7d9db61bdb6254478e7414d9aac121e
Ines Montani <ines@ines.io>
2020-01-07 01:39:24 +0100

    Drop unused utils and update MNIST

--- ml_datasets/mnist.py
+++ ml_datasets/mnist.py
@@ -1,26 +1,45 @@
 import random
+import gzip
+from srsly import cloudpickle as pickle
 
+from .util import unzip, to_categorical
+from ._vendorized.keras_data_utils import get_file
 from ._registry import register_loader
+
+
+URL = "https://s3.amazonaws.com/img-datasets/mnist.pkl.gz"
 
 
 @register_loader("mnist")
 def mnist():
-    from ._vendorized.keras_datasets import load_mnist
-
     # the data, shuffled and split between tran and test sets
     (X_train, y_train), (X_test, y_test) = load_mnist()
-
     X_train = X_train.reshape(60000, 784)
     X_test = X_test.reshape(10000, 784)
     X_train = X_train.astype("float32")
     X_test = X_test.astype("float32")
-
     X_train /= 255.0
     X_test /= 255.0
     train_data = list(zip(X_train, y_train))
     nr_train = X_train.shape[0]
     random.shuffle(train_data)
     heldout_data = train_data[: int(nr_train * 0.1)]
-    train_data = train_data[len(heldout_data) :]
-    test_data = list(zip(X_test, y_test))
-    return train_data, heldout_data, test_data
+    mnist_train = train_data[len(heldout_data) :]
+    mnist_dev = list(zip(X_test, y_test))
+
+    train_X, train_Y = unzip(mnist_train)
+    train_Y = to_categorical(train_Y, n_classes=10)
+    dev_X, dev_Y = unzip(mnist_dev)
+    dev_Y = to_categorical(dev_Y, n_classes=10)
+    return (train_X, train_Y), (dev_X, dev_Y)
+
+
+def load_mnist(path="mnist.pkl.gz"):
+    path = get_file(path, origin=URL)
+    if path.endswith(".gz"):
+        f = gzip.open(path, "rb")
+    else:
+        f = open(path, "rb")
+    data = pickle.load(f, encoding="bytes")
+    f.close()
+    return data  # (X_train, y_train), (X_test, y_test)
--- ml_datasets/util.py
+++ ml_datasets/util.py
@@ -1,4 +1,87 @@
 import numpy
+import tarfile
+import zipfile
+import os
+import shutil
+from urllib.error import URLError, HTTPError
+from urllib.request import urlretrieve
+import tqdm
+
+
+def get_file(fname, origin, untar=False, unzip=False, cache_subdir="datasets"):
+    """Downloads a file from a URL if it not already in the cache."""
+    # https://raw.githubusercontent.com/fchollet/keras/master/keras/utils/data_utils.py
+    # Copyright Francois Chollet, Google, others (2015)
+    # Under MIT license
+    datadir_base = os.path.expanduser(os.path.join("~", ".keras"))
+    if not os.access(datadir_base, os.W_OK):
+        datadir_base = os.path.join("/tmp", ".keras")
+    datadir = os.path.join(datadir_base, cache_subdir)
+    if not os.path.exists(datadir):
+        os.makedirs(datadir)
+    if untar or unzip:
+        untar_fpath = os.path.join(datadir, fname)
+        if unzip:
+            fpath = untar_fpath + ".zip"
+        else:
+            fpath = untar_fpath + ".tar.gz"
+    else:
+        fpath = os.path.join(datadir, fname)
+    print("Downloading data from", origin)
+    global progbar
+    progbar = None
+
+    def dl_progress(count, block_size, total_size):
+        global progbar
+        if progbar is None:
+            progbar = tqdm.tqdm(total=total_size)
+        else:
+            progbar.update(block_size)
+
+    error_msg = "URL fetch failure on {}: {} -- {}"
+    try:
+        try:
+            urlretrieve(origin, fpath, dl_progress)
+        except URLError as e:
+            raise Exception(error_msg.format(origin, e.errno, e.reason))
+        except HTTPError as e:
+            raise Exception(error_msg.format(origin, e.code, e.msg))
+    except (Exception, KeyboardInterrupt):
+        if os.path.exists(fpath):
+            os.remove(fpath)
+        raise
+    progbar = None
+
+    if untar:
+        if not os.path.exists(untar_fpath):
+            print("Untaring file...")
+            tfile = tarfile.open(fpath, "r:gz")
+            try:
+                tfile.extractall(path=datadir)
+            except (Exception, KeyboardInterrupt):
+                if os.path.exists(untar_fpath):
+                    if os.path.isfile(untar_fpath):
+                        os.remove(untar_fpath)
+                    else:
+                        shutil.rmtree(untar_fpath)
+                raise
+            tfile.close()
+        return untar_fpath
+    elif unzip:
+        if not os.path.exists(untar_fpath):
+            print("Unzipping file...")
+            with zipfile.ZipFile(fpath) as file_:
+                try:
+                    file_.extractall(path=datadir)
+                except (Exception, KeyboardInterrupt):
+                    if os.path.exists(untar_fpath):
+                        if os.path.isfile(untar_fpath):
+                            os.remove(untar_fpath)
+                        else:
+                            shutil.rmtree(untar_fpath)
+                    raise
+        return untar_fpath
+    return fpath
 
 
 def partition(examples, split_size):
@@ -7,3 +90,19 @@ def partition(examples, split_size):
     n_docs = len(examples)
     split = int(n_docs * split_size)
     return examples[:split], examples[split:]
+
+
+def unzip(data):
+    x, y = zip(*data)
+    return numpy.asarray(x), numpy.asarray(y)
+
+
+def to_categorical(Y, n_classes=None):
+    # From keras
+    Y = numpy.array(Y, dtype="int").ravel()
+    if not n_classes:
+        n_classes = numpy.max(Y) + 1
+    n = Y.shape[0]
+    categorical = numpy.zeros((n, n_classes), dtype="float32")
+    categorical[numpy.arange(n), Y] = 1
+    return numpy.asarray(categorical)
