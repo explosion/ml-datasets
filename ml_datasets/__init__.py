94d9de1635e9b944b1d03caa2f676746d74b35b8
svlandeg <sofie.vanlandeghem@gmail.com>
2020-09-14 17:15:18 +0200

    fix init

--- ml_datasets/__init__.py
+++ ml_datasets/__init__.py
@@ -1,2 +1,10 @@
 from ._registry import register_loader as loaders
-from ml_datasets.loaders.stack_exchange import stack_exchange
+from .loaders.imdb import imdb
+from .loaders.mnist import mnist
+from .loaders.quora import quora_questions
+from .loaders.reuters import reuters
+from .loaders.snli import snli
+from .loaders.stack_exchange import stack_exchange
+from .loaders.universal_dependencies import ud_ancora_pos_tags, ud_ewtb_pos_tags
+from .loaders.dbpedia import dbpedia
+from .loaders.cmu import cmu
