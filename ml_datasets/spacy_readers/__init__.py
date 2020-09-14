a92802d86177da012cc3c46988ff14d0ed9763ee
svlandeg <sofie.vanlandeghem@gmail.com>
2020-09-14 17:00:04 +0200

    put spacy readers in separate dir

---
+++ ml_datasets/spacy_readers/__init__.py
@@ -0,0 +1,3 @@
+from .imdb_reader import imdb_reader
+from .cmu_reader import cmu_reader
+from .dbpedia_reader import dbpedia_reader
---
+++ ml_datasets/spacy_readers/dbpedia_reader.py
@@ -0,0 +1,27 @@
+from typing import Iterable, Callable
+from pathlib import Path
+from spacy.training.example import Example
+
+from ..loaders import dbpedia
+
+
+def dbpedia_reader(
+    train: bool, path: Path = None, *, limit: int = 0
+) -> Callable[["Language"], Iterable["Example"]]:
+    from spacy.training.example import Example
+
+    all_train_data = dbpedia(train=True, loc=path, limit=0)
+    unique_labels = set()
+    for text, gold_label in all_train_data:
+        assert isinstance(gold_label, str)
+        unique_labels.add(gold_label)
+    # do this here to avoid reading the data multiple times
+    data = list(dbpedia(train, path, limit=limit))
+
+    def read_examples(nlp):
+        for text, gold_label in data:
+            doc = nlp.make_doc(text)
+            cat_dict = {label: float(gold_label == label) for label in unique_labels}
+            yield Example.from_dict(doc, {"cats": cat_dict})
+
+    return read_examples
