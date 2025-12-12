b2ba9c8e063ef1a1764ef85f2d2ed7777bda13ef
crusaderky <crusaderky@gmail.com>
2025-12-12 15:21:43 +0000

    Use a single TODO at the top of the file

--- ml_datasets/test/test_datasets.py
+++ ml_datasets/test/test_datasets.py
@@ -1,3 +1,6 @@
+# TODO the tests below only verify that the various functions don't crash.
+# Expand them to test the actual output contents.
+
 import platform
 
 import pytest
@@ -24,67 +27,55 @@ else:
 @np_24_deprecation
 def test_cifar():
     (X_train, y_train), (X_test, y_test) = ml_datasets.cifar()
-    # TODO test output contents
 
 
 @pytest.mark.skip(reason="very slow download")
 def test_cmu():
     train, dev = ml_datasets.cmu()
-    # TODO test output contents
 
 
 def test_dbpedia():
     train, dev = ml_datasets.dbpedia()
-    # TODO test output contents
 
 
 def test_imdb():
     train, dev = ml_datasets.imdb()
-    # TODO test output contents
 
 
 @np_24_deprecation
 def test_mnist():
     (X_train, y_train), (X_test, y_test) = ml_datasets.mnist()
-    # TODO test output contents
 
 
 @pytest.mark.xfail(reason="403 Forbidden")
 def test_quora_questions():
     train, dev = ml_datasets.quora_questions()
-    # TODO test output contents
 
 
 @np_24_deprecation
 def test_reuters():
     (X_train, y_train), (X_test, y_test) = ml_datasets.reuters()
-    # TODO test output contents
 
 
 @pytest.mark.xfail(platform.system() == "Windows", reason="path issues")
 def test_snli():
     train, dev = ml_datasets.snli()
-    # TODO test output contents
 
 
 @pytest.mark.xfail(reason="no default path")
 def test_stack_exchange():
     train, dev = ml_datasets.stack_exchange()
-    # TODO test output contents
 
 
 def test_ud_ancora_pos_tags():
     (train_X, train_y), (dev_X, dev_y) = ml_datasets.ud_ancora_pos_tags()
-    # TODO test output contents
 
 
 @pytest.mark.xfail(reason="str column where int expected")
 def test_ud_ewtb_pos_tags():
     (train_X, train_y), (dev_X, dev_y) = ml_datasets.ud_ewtb_pos_tags()
-    # TODO test output contents
 
 
 @pytest.mark.xfail(reason="no default path")
 def test_wikiner():
     train, dev = ml_datasets.wikiner()
-    # TODO test output contents
